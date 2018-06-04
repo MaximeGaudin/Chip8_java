c1444e1b942b03c7c332490397140ccba307b343
Maxime Gaudin <gaudin.maxime@gmail.com>
2018-06-04 21:24:39 +0200

    Revert method before forgetting it

--- src/main/java/com/mgaudin/chip8/Keyboard.java
+++ src/main/java/com/mgaudin/chip8/Keyboard.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class Keyboard {
     public boolean isPressed(byte keyCode) {
-        return false;
+        throw new RuntimeException("Not implemented");
     }
 
     public byte blockUntilKeyPress() {
